83c10a0fe80f7df025b0c4f694863cd8433284d3
kaspii314 <kate.stadelman@du.edu>
2022-01-08 14:34:02 -0600

    completed scaled sigmoid

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def nth_power(n, fn=lambda x: x**2):
     '''
     calculates power for integers up to n
@@ -7,5 +9,8 @@ def nth_power(n, fn=lambda x: x**2):
     '''
     return [ fn(i) for i in range(n) ]
 
+def sigmoid(x, a=1):
+    return 1/(1 + np.exp(-a*x))
 
 print(nth_power(10))
+print(sigmoid(0))
